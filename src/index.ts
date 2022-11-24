9fde6eee0d3f675b628168eddb4cd2274dbbbd83
TimPrevotPro <timothe.prevot@efrei.net>
2022-11-24 15:38:32 +0100

    Second test is working

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entities/user"
+import {AppDataSource} from "./data-source"
+import {User} from "./entities/user"
 
 AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
-    await createUserTest();
+    await createUser("Timber", "Saw", 'test', 'passwordtest');
 }).catch(error => console.log(error))
 
-export async function createUserTest(): Promise<User | null> {
+export async function createUser(firstName: string, lastName: string, email: string, passwordHash: string): Promise<User | null | Error> {
     const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.email = "test"
-    user.passwordHash = "passwordtest"
-    await AppDataSource.getRepository(User).save(user)
-    console.log("Saved a new user with id: ", user.id)
-    const savedUser: User | null = await AppDataSource.getRepository(User).findOneBy({ id: user.id });
-    return savedUser;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.passwordHash = passwordHash;
+    if (!user.email || !user.passwordHash || user.email === '' || user.passwordHash === '') {
+        console.log('raising error...');
+        throw new Error('email or password is invalid.');
+    } else {
+        await AppDataSource.getRepository(User).save(user)
+        console.log("Saved a new user with id: ", user.id)
+        const savedUser: User | null = await AppDataSource.getRepository(User).findOneBy({id: user.id});
+        return savedUser;
+    }
 }
--- src/specs/entities/user.spec.ts
+++ src/specs/entities/user.spec.ts
@@ -2,7 +2,7 @@ import {describe, test, expect, beforeAll, beforeEach} from "@jest/globals";
 import {AppDataSource} from "../../data-source";
 import {User} from "../../entities/user";
 import {Repository} from "typeorm";
-import {createUserTest} from "../../index";
+import {createUser} from "../../index";
 
 import {dropUserTable} from "../helper";
 
@@ -16,16 +16,18 @@ describe('user module', () => {
         });
     });
 
-    beforeEach( async () => {
+    beforeEach(async () => {
         console.log("Dropping user table...");
         await dropUserTable();
         console.log('user table dropped successfully.')
     });
 
-    describe('Validations', () => {
-        test('Should create a new user in the database', async () => {
-            expect(await createUserTest()).toStrictEqual(new User(await userRepository.count(), "Timber", "Saw", "test", "passwordtest"));
-        });
-
+    test('Should create a new user in the database', async () => {
+        expect(await createUser('Timber', 'Saw', 'test', 'passwordtest')).toStrictEqual(new User(await userRepository.count(), "Timber", "Saw", "test", "passwordtest"));
+    });
+    test('Should raise error if email is missing', () => {
+        void expect(async () => {
+            await createUser('Timber2', 'Saw', '', 'passwordtest');
+        }).rejects.toThrow(Error);
     })
 })
